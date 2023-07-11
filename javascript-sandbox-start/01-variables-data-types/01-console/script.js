1c4ea7197431432330cb6ac7372520fe84568a95
Carlos Pereira <carlospereira.ti@gmail.com>
2023-07-10 22:09:19 -0300

    initial commit

---
+++ javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -0,0 +1,19 @@
+
+console.log(100);
+console.log('Hello from the JS console');
+console.log('Hello', 100, true);
+
+const x = 100;
+console.log(x);
+
+console.error('Alert');
+console.warn('Warning');
+console.table({name: 'Charlie', email: 'carlospereira.ti@gmail.com'});
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
+
+
+
+
