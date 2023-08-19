d164d616fd08a70e5930016ffd1e7e0f0d37aa6d
Carlos Pereira <carlospereira.ti@gmail.com>
2023-08-19 10:04:52 -0300

    arrays basics

---
+++ javascript-sandbox-start/02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,27 @@
+// ! Arrays Basics
+
+let x;
+
+// Array literal
+const numbers = [12,45,33,29,39];
+const mixed = [25, 'Hello', true, null];
+
+console.log(numbers);
+
+
+// Array constructor
+const fruits = new Array('apple','banana','grape','orange');
+
+x = numbers[0];
+x = numbers[0] + numbers[2];
+x = `My favorite fruit is an ${fruits[3]}`;
+
+x = mixed.length;
+
+
+console.log(fruits);
+console.log(x);
+
+
+
+
